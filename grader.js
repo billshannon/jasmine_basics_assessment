70876997fe7ed56fdc896034da970261169e220b
Bill Shannon <bill@billshannon.com>
2015-08-08 12:39:08 -0600

    letter grader test passed

--- grader.js
+++ grader.js
@@ -1,3 +1,13 @@
 exports.outputLetterGrader = function (grade) {
-    return 'A';
+    if (grade <= 100 && grade >= 90) {
+        return 'A';
+    } else if (grade <= 89 && grade >= 80) {
+        return 'B';
+    } else if (grade <= 79 && grade >= 70) {
+        return 'C';
+    } else if (grade <= 69 && grade >= 60) {
+        return 'D';
+    } else if (grade <= 59 && grade >= 0) {
+        return 'F'
+    }
 };
--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,11 @@
 var code = require('../grader.js');
 
-
 describe('Letter Grader', function () {
     it('should take a test score and return the equivalent letter grade', function () {
-        expect(code.outputLetterGrader(98)).toEqual('A');
+        expect(code.outputLetterGrader(99)).toEqual('A');
+        expect(code.outputLetterGrader(88)).toEqual('B');
+        expect(code.outputLetterGrader(77)).toEqual('C');
+        expect(code.outputLetterGrader(66)).toEqual('D');
+        expect(code.outputLetterGrader(55)).toEqual('F');
     })
 });
