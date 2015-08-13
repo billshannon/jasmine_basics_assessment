cc001a83ef6f3aed1e9976ec320b8a1a0aee593b
Bill Shannon <bill@billshannon.com>
2015-08-13 16:34:51 -0600

    refactored letter grader if statements

--- grader.js
+++ grader.js
@@ -1,13 +1,13 @@
 exports.outputLetterGrader = function (grade) {
-    if (grade <= 100 && grade >= 90) {
+    if (grade >= 90) {
         return 'A';
-    } else if (grade <= 89 && grade >= 80) {
+    } else if (grade >= 80) {
         return 'B';
-    } else if (grade <= 79 && grade >= 70) {
+    } else if (grade >= 70) {
         return 'C';
-    } else if (grade <= 69 && grade >= 60) {
+    } else if (grade >= 60) {
         return 'D';
-    } else if (grade <= 59 && grade >= 0) {
+    } else if (grade >= 0) {
         return 'F'
     }
 };
